6c750f769bc10d3234299cf3ca840f76c06cead6
Denis Moraes Rêgo Campos <denismrc8@gmail.com>
2018-01-11 22:44:05 -0300

    adicionando email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,63 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Bootstrap core CSS-->
+    <link href="{{ URL::asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="{{ URL::asset('vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template-->
+    <link href="{{ URL::asset('css/sb-admin.css') }}" rel="stylesheet">
+</head>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<body class="bg-dark">
+  <div class="container">
+    <div class="card card-login mx-auto mt-5">
+      <div class="card-header">Resetar senha</div>
+      <div class="card-body">
+        <div class="text-center mt-4 mb-5">
+          <h4>Esqueceu a senha?</h4>
+          <p>Entre com seu e-mail e siga as instruções.</p>
         </div>
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
+            {{ csrf_field() }}
+            <div  class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Enter email address" required>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">
+                Resetar senha
+            </button>
+        </form>
+        <div class="text-center">
+          <a class="d-block small mt-3" href="{{ route('register') }}">Cadastrar</a>
+          <a class="d-block small" href="{{ route('login') }}">Página de Login</a>
+        </div>
+      </div>
     </div>
-</div>
-@endsection
+  </div>
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{ URL::asset('vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ URL::asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="{{ URL::asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+</body>
+
+</html>
